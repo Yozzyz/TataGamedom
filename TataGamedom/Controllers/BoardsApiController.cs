9e5ef6274dd0c325b5be5a38c3c95ade12d80fad
Jiaen Lee <nn750137@gmail.com>
2023-06-29 23:10:57 +0800

    Complete BoardApiText -> Index (a.k.a. List)

---
+++ TataGamedom/Controllers/BoardsApiController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TataGamedom.Models.EFModels;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using TataGamedom.Models.Dtos.Boards;
+
+namespace TataGamedom.Controllers
+{
+    public class BoardsApiController : ApiController
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: api/BoardsApi
+        public async Task<IEnumerable<BoardListDto>> GetBoards()
+        {
+            if (db.Boards == null)
+            {
+                return null;
+            }
+            return db.Boards.Select(bd => new BoardListDto
+            {
+                Name = bd.Name,
+                BoardAboud = bd.BoardAbout
+            });
+        }
+
+        // GET: api/BoardsApi/
+        [ResponseType(typeof(Board))]
+        public IHttpActionResult GetBoard(int id)
+        {
+            Board board = db.Boards.Find(id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(board);
+        }
+
+        // PUT: api/BoardsApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBoard(int id, Board board)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != board.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(board).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BoardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/BoardsApi
+        [ResponseType(typeof(Board))]
+        public IHttpActionResult PostBoard(Board board)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Boards.Add(board);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = board.Id }, board);
+        }
+
+        // DELETE: api/BoardsApi/5
+        [ResponseType(typeof(Board))]
+        public IHttpActionResult DeleteBoard(int id)
+        {
+            Board board = db.Boards.Find(id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            db.Boards.Remove(board);
+            db.SaveChanges();
+
+            return Ok(board);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BoardExists(int id)
+        {
+            return db.Boards.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ TataGamedom/Models/Dtos/Boards/BoardListDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TataGamedom.Models.Dtos.Boards
+{
+	public class BoardListDto
+	{
+		public string Name { get; set; }
+		public string BoardAboud { get; set; }
+	}
+}
