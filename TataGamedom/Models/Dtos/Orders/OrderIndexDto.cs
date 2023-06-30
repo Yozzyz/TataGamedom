317b658582fedbef6727bf93ec2f3a3d0d392256
apalan60 <apalan60@gmail.com>
2023-06-30 11:56:16 +0800

    新增Orders 新增ReadMe資料夾 刪除測試用Class1

---
+++ TataGamedom/Models/Dtos/Orders/OrderIndexDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TataGamedom.Models.ViewModels.Orders;
+
+namespace TataGamedom.Models.Dtos.Orders
+{
+	public class OrderIndexDto
+	{
+		public string Index { get; set; }
+
+		public string MemberName { get; set; }
+
+		public string OrderStatusCodeName { get; set; }
+
+		public string ShipmentStatusCodeName { get; set; }
+
+		public string PaymentStatusCodeName { get; set; }
+
+		public DateTime CreatedAt { get; set; }
+
+		public int Total { get; set; }
+	}
+	public static class OrderIndexExts
+	{
+		//public static OrderIndexDto ToDto(this Order entity)
+		//{
+		//}
+		public static OrderIndexVM ToIndexVM(this OrderIndexDto dto)
+		{
+			return new OrderIndexVM
+			{
+				Index = dto.Index,
+				MemberName = dto.MemberName,
+				OrderStatusCodeName = dto.OrderStatusCodeName,
+				ShipmentStatusCodeName = dto.ShipmentStatusCodeName,
+				PaymentStatusCodeName = dto.PaymentStatusCodeName,
+				CreatedAt = dto.CreatedAt,
+				Total = dto.Total
+			};
+		}
+	}
+
+}
---
+++ TataGamedom/Models/Interfaces/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using TataGamedom.Models.Dtos.Orders;
+using static TataGamedom.Models.Services.OrderService;
+
+namespace TataGamedom.Models.Interfaces
+{
+	public interface IOrderRepository
+	{
+		IEnumerable<OrderIndexDto> Search(Criteria criteria, SortInfo sortInfo);
+
+		IEnumerable<OrderIndexDto> Search(Criteria criteria, SortInfo sortInfo, string sqlAddPage);
+	}
+}
