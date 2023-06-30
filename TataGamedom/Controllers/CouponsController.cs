e6fdf7e03552234b7aaf8cd4ea6af1899aa1357d
Yiyier <iamkellyoung@gmail.com>
2023-06-30 16:32:06 +0800

    新增優惠券、商品Controller 新增優惠券、商品Index

---
+++ TataGamedom/Controllers/CouponsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TataGamedom.Models.Infra.DapperRepositories;
+using TataGamedom.Models.Interfaces;
+using TataGamedom.Models.Services;
+using TataGamedom.Models.ViewModels.Coupons;
+
+namespace TataGamedom.Controllers
+{
+    public class CouponsController : Controller
+    {
+        [Authorize]
+        // GET: Coupons
+        public ActionResult Index()
+        {
+            IEnumerable<CouponIndexVM> coupons = GetCoupons();
+            return View(coupons);
+        }
+
+		private IEnumerable<CouponIndexVM> GetCoupons()
+		{
+			ICouponsRepository repo = new CouponDapperRepository();
+            CouponService service = new CouponService(repo);
+            return service.Get();
+		}
+	}
+}
---
+++ TataGamedom/Models/Infra/DapperRepositories/ProductDapperRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+using TataGamedom.Models.Interfaces;
+using TataGamedom.Models.ViewModels.Products;
+
+namespace TataGamedom.Models.Infra.DapperRepositories
+{
+	public class ProductDapperRepository : IProductRepository
+	{
+		private string _connStr;
+		public ProductDapperRepository()
+		{
+			_connStr = System.Configuration.ConfigurationManager.ConnectionStrings["AppDbContext"].ToString();
+		}
+
+		public IEnumerable<ProductIndexVM> Get()
+		{
+			using(var conn = new SqlConnection(_connStr))
+			{
+				string sql = @"SELECT P.[Index] AS [Index],G.ChiName AS GameName ,GPC.Name AS GamePlatformName,P.IsVirtual AS IsVirtual,P.Price AS Price,CONVERT(DATE,P.SaleDate)AS SaleDate,PSC.Name AS ProductStatus,BM.Name AS CreatedBackendMemberName,P.CreatedTime AS CreatedTime
+FROM Products AS P
+JOIN Games AS G ON P.GameId=G.Id
+JOIN GamePlatformsCodes AS GPC ON GPC.Id = P.GamePlatformId
+JOIN ProductStatusCodes AS PSC ON PSC.Id = P.ProductStatusId
+JOIN BackendMembers AS BM ON BM.Id = P.CreatedBackendMemberId";
+				return conn.Query<ProductIndexVM>(sql);
+			}
+		}
+	}
+}
