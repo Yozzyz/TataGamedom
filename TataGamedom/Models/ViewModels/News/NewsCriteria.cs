a00dafdd2bc29a8ed7cdce1fa9643a93722fcac7
fuen28 <zzzz86115@gmail.com>
2023-06-30 16:39:40 +0800

    news新增查詢

---
+++ TataGamedom/Models/ViewModels/News/NewsCriteria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TataGamedom.Models.ViewModels.News
+{
+	public class NewsCriteria
+	{
+		public string Title { get; set; }
+		public string Name { get; set; }
+
+		public int? GamesId { get; set; }
+
+		public DateTime ScheduleDate { get; set; }
+
+		public bool ActiveFlag { get; set; }
+
+
+
+	}
+}
