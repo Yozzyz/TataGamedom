add6ede8cfdd4edaa13ea2c16a258507553a3e52
fuen28 <zzzz86115@gmail.com>
2023-07-03 11:18:26 +0800

    0703KUAN更新
    NEWS修改

--- TataGamedom/Controllers/NewsCommentsController.cs
+++ TataGamedom/Controllers/NewsCommentsController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using TataGamedom.Models.EFModels;
 using TataGamedom.Models.ViewModels.News;
 using Dapper;
+using TataGamedom.Models.ViewModels.Members;
 
 namespace TataGamedom.Controllers
 {
@@ -22,7 +23,7 @@ namespace TataGamedom.Controllers
 		{
 			using (var con = new SqlConnection(_connstr))
 			{
-				string sql = @"SELECT M.Name as MemberName, NC.Time, NC.Content, NC.ActiveFlag, N.Title as Title
+				string sql = @"SELECT NC.Id ,M.Name as MemberName, NC.Time, NC.Content, NC.ActiveFlag, N.Title as Title
 FROM NewsComments AS NC
 JOIN Members AS M ON M.Id = NC.MemberID
 JOIN News AS N ON N.Id = NC.NewsId
@@ -41,6 +42,65 @@ JOIN News AS N ON N.Id = NC.NewsId
 				}
 			}
 		}
+
+		public ActionResult Details(int? id)
+		{
+			using (var con = new SqlConnection(_connstr))
+			{
+				string sql = @"SELECT NC.Id, M.Name as MemberName, NC.Time, NC.Content, NC.ActiveFlag, N.Title as Title
+FROM NewsComments AS NC
+JOIN Members AS M ON M.Id = NC.MemberID
+JOIN News AS N ON N.Id = NC.NewsId
+WHERE NC.Id = @Id";
+
+				var detials = con.Query<NewsCommentIndexVM>(sql, new { Id = id }).SingleOrDefault();
+
+				return View(detials);
+			}
+		}
+
+
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			var currentUserAccount = User.Identity.Name;
+			var backendMember = db.BackendMembers.FirstOrDefault(m => m.Account == currentUserAccount);
+
+			if (backendMember != null)
+			{
+				using (var con = new SqlConnection(_connstr))
+				{
+					string sql = @"UPDATE NewsComments SET ActiveFlag = 0, DeleteDatetime = GETDATE(), DeleteBackendMemberId = @BackendMemberId WHERE Id = @Id";
+
+					con.Execute(sql, new { BackendMemberId = backendMember.Id, Id = id });
+				}
+			}
+			return RedirectToAction("Index");
+		}
+
+
+
+		[HttpPost, ActionName("Reduction")]
+		[ValidateAntiForgeryToken]
+		public ActionResult ReductionConfirmed(int id)
+		{
+			var currentUserAccount = User.Identity.Name;
+			var backendMember = db.BackendMembers.FirstOrDefault(m => m.Account == currentUserAccount);
+
+			if (backendMember != null)
+			{
+				using (var con = new SqlConnection(_connstr))
+				{
+					string sql = @"UPDATE NewsComments SET ActiveFlag = 1, DeleteDatetime = NULL, DeleteBackendMemberId = NULL WHERE Id = @Id";
+
+					con.Execute(sql, new { BackendMemberId = backendMember.Id, Id = id });
+				}
+			}
+			return RedirectToAction("Index");
+		}
+
 
 	}
 }
